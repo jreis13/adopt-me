59ae83a51810a30928b83f8b071fa915a179aa35
João Francisco Reis <joaofranciscoreis13@outlook.com>
2022-03-30 19:36:41 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="title">Adopt Me!</h1>
-      <Pet name="Kobe" animal="Dog" breed="Staffordshire" />
-      <Pet name="Mia" animal="Cat" breed="Shorthair" />
-      <Pet name="Gaspar" animal="Dog" breed="Beagle" />
+      <SearchParams />
     </div>
   );
 };
